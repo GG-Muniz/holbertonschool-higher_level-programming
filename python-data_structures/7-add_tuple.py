48aea8aee6fc54cdce21b4a018a4971399682686
GG-Muniz <g.garciamuniz@outlook.com>
2025-05-16 16:45:06 -0400

    Python Data Structure project and tasks.

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure both tuples have at least 2 elements (use 0 for missing ones)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    
+    # Return a new tuple with the sum of first and second elements
+    return (a[0] + b[0], a[1] + b[1])
