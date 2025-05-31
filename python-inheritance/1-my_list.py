f12020c5ef4dd451bdc29ccfba659ef2b9f52d3d
GG-Muniz <g.garciamuniz@outlook.com>
2025-05-31 16:22:42 -0400

    Adding new my_list python file version as well as new txt file version to pass test checker.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""
-MyList module
+"""MyList module.
+
+This module defines a MyList class that inherits from list.
 """
 
 
 class MyList(list):
-    """
-    MyList class that inherits list
+    """MyList class that inherits from list.
+
+    This class inherits all the functionality of the built-in list class
+    and adds a method to print the list in sorted order.
     """
 
     def print_sorted(self):
-        """
-        Prints the list but sorted (ascending sort)
+        """Print the list but sorted (ascending sort).
+
+        This method prints the list in ascending order without modifying
+        the original list.
         """
         print(sorted(self))
