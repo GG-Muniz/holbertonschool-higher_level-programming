2832687710efb9ba6d22641d9e9cf7654b94ab07
GG-Muniz <g.garciamuniz@outlook.com>
2025-06-07 14:23:25 -0400

    Fixing module documentation.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Module that contains a function to read a text file."""
+
+
 def read_file(filename=""):
-    """Read a text file (UTF8) and print it to stdout."""
+    """Read a text file (UTF8) and print it to stdout.
+    
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
