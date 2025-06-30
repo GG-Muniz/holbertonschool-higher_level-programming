612926277260c9056bb50b159010e55402921d54
GG-Muniz <g.garciamuniz@outlook.com>
2025-06-29 20:23:34 -0400

    Fixing correct output.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values in the states table where name matches the argument"""
-
+"""Displays all values in the states table where name matches the argument"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
     # Connect to MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     # Create cursor object
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    # Execute SQL query using format (vulnerable to SQL injection)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
+    # Execute query using format (vulnerable to SQL injection)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(query)
 
     # Fetch all results
-    results = cursor.fetchall()
+    rows = cur.fetchall()
 
     # Print results
-    for row in results:
+    for row in rows:
         print(row)
 
     # Close cursor and database connection
-    cursor.close()
+    cur.close()
     db.close()
