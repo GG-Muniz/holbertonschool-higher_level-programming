2b7305d53a7b3d7307b6242b65148945ae56783e
GG-Muniz <g.garciamuniz@outlook.com>
2025-06-29 20:27:17 -0400

    Added state_id Column line.

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""Module that contains the class definition of a State and an instance Base = declarative_base()"""
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-# Create Base instance
-Base = declarative_base()
+"""Contains the class definition of a City"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
-class State(Base):
-    """State class that represents the states table"""
-    __tablename__ = 'states'
+class City(Base):
+    """City class that links to the MySQL table cities"""
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
