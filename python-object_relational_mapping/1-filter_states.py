368e39c1356fd60a465043311b22b9bdf11b15b1
GG-Muniz <g.garciamuniz@outlook.com>
2025-06-29 20:18:37 -0400

    Refractor - chasing requirement checker not passing #7.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N.
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,19 +22,12 @@ if __name__ == "__main__":
         charset="utf8"
     )
 
-    # Create cursor object
     cursor = db.cursor()
-
-    # Execute SQL query to filter states starting with 'N'
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all results
     results = cursor.fetchall()
 
-    # Print results
     for row in results:
         print(row)
 
-    # Close cursor and database connection
     cursor.close()
     db.close()
